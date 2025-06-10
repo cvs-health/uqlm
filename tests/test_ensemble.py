10cd3bd71463287fcd43ec599aef23b0f05e0f9a
2613037 <mohitsingh.chauhan@cvshealth.com>
2025-06-10 10:19:29 -0400

    additional tests cases to achieve 100% test coverage

--- tests/test_ensemble.py
+++ tests/test_ensemble.py
@@ -22,8 +22,8 @@ datafile_path = "tests/data/scorers/ensemble_results_file.json"
 with open(datafile_path, "r") as f:
     expected_result = json.load(f)
 
-data = expected_result["ensemble"]["data"]
-metadata = expected_result["ensemble"]["metadata"]
+data = expected_result["ensemble1"]["data"]
+metadata = expected_result["ensemble1"]["metadata"]
     
 PROMPTS = data["prompts"]
 MOCKED_RESPONSES = data["responses"]
@@ -65,6 +65,26 @@ def test_wrong_components(mock_llm):
         )
     assert "Components must be an instance of LLMJudge, BaseChatModel" in str(value_error.value)
 
+@pytest.mark.asyncio
+async def test_error_sampled_response(mock_llm):
+    with pytest.raises(ValueError) as value_error:
+        uqe = UQEnsemble(
+            llm=mock_llm,
+            scorers=["exact_match"]
+        )
+        await uqe.score(prompts=PROMPTS, responses=MOCKED_RESPONSES)
+    assert "sampled_responses must be provided if using black-box scorers" == str(value_error.value)
+
+@pytest.mark.asyncio
+async def test_error_logprobs_results(mock_llm):
+    with pytest.raises(ValueError) as value_error:
+        uqe = UQEnsemble(
+            llm=mock_llm,
+            scorers=["min_probability"]
+        )
+        await uqe.score(prompts=PROMPTS, responses=MOCKED_RESPONSES)
+    assert "logprobs_results must be provided if using white-box scorers" == str(value_error.value)
+
 def test_wrong_weights(mock_llm):
     with pytest.raises(ValueError) as value_error:
         UQEnsemble(llm=mock_llm, scorers=["exact_match"], weights=[0.5, 0.5])
@@ -72,7 +92,7 @@ def test_wrong_weights(mock_llm):
 
 def test_bsdetector_weights(mock_llm):
     uqe = UQEnsemble(llm=mock_llm)
-    assert (uqe.weights == [0.7 * 0.8, 0.7 * 0.2, 0.3]).all()
+    assert uqe.weights == [0.7 * 0.8, 0.7 * 0.2, 0.3]
 
 @pytest.mark.asyncio
 async def test_ensemble(monkeypatch, mock_llm):
@@ -145,3 +165,92 @@ async def test_ensemble(monkeypatch, mock_llm):
     )
 
     assert results.metadata == metadata
+
+    tune_results = {"weights": [0.5, 0.2, 0.3], "thresh": 0.75}
+
+    def mock_tune_params(*args, **kwargs):
+        return tune_results
+    
+    uqe = UQEnsemble(
+        llm=mock_llm,
+        scorers=["exact_match", "noncontradiction", mock_llm]
+    )
+
+    monkeypatch.setattr(uqe.tuner, "tune_params", mock_tune_params)
+    monkeypatch.setattr(uqe, "generate_original_responses", mock_generate_original_responses)
+    monkeypatch.setattr(uqe, "generate_candidate_responses", mock_generate_candidate_responses)
+    monkeypatch.setattr(uqe.judges_object, "score", mock_judge_scores)
+
+    result = await uqe.tune(prompts=PROMPTS, 
+                   ground_truth_answers=PROMPTS)
+    assert result.metadata["weights"] == tune_results["weights"]
+    assert result.metadata["thresh"] == tune_results["thresh"]
+
+    result = await uqe.tune(prompts=PROMPTS, 
+                   ground_truth_answers=[PROMPTS[0]]+[" "]*len(PROMPTS[:-1]),
+                   grader_function=lambda response,answer: response==answer)
+    assert result.metadata["thresh"] == tune_results["thresh"]
+
+
+@pytest.mark.asyncio
+async def test_ensemble2(monkeypatch, mock_llm):
+    data = expected_result["ensemble2"]["data"]
+    metadata = expected_result["ensemble2"]["metadata"]
+        
+    PROMPTS = data["prompts"]
+    MOCKED_RESPONSES = data["responses"]
+    MOCKED_JUDGE_SCORES = data['judge_1']
+    MOCKED_LOGPROBS = metadata["logprobs"]
+    uqe = UQEnsemble(
+        llm=mock_llm,
+        scorers=[
+            "min_probability",  
+            mock_llm,  
+        ]
+    )
+    
+    async def mock_generate_original_responses(*args, **kwargs):
+        uqe.logprobs = MOCKED_LOGPROBS
+        return MOCKED_RESPONSES
+    
+    async def mock_judge_scores(*args, **kwargs):
+        return UQResult({'data': {'judge_1': MOCKED_JUDGE_SCORES}})
+    
+    monkeypatch.setattr(uqe, "generate_original_responses", mock_generate_original_responses)
+    monkeypatch.setattr(uqe.judges_object, "score", mock_judge_scores)
+    
+    results = await uqe.generate_and_score(
+        prompts=PROMPTS,
+    )
+
+    assert all(
+        [
+            results.data["min_probability"][i]
+            == pytest.approx(data["min_probability"][i])
+            for i in range(len(PROMPTS))
+        ]
+    )
+
+    assert all(
+        [
+            abs(results.data["judge_1"][i] - data["judge_1"][i]) < 1e-5
+            for i in range(len(PROMPTS))
+        ]
+    )
+
+    assert results.metadata == metadata
+
+@pytest.mark.asyncio
+async def test_default_logprob(monkeypatch, mock_llm):
+
+    async def mock_judge_scores(*args, **kwargs):
+        return UQResult({'data': {'judge_1': MOCKED_JUDGE_SCORES}})
+    
+    uqe = UQEnsemble(
+        llm=mock_llm,
+        scorers=[mock_llm]
+    )
+    monkeypatch.setattr(uqe.judges_object, "score", mock_judge_scores)
+    await uqe.score(prompts=PROMPTS, responses=MOCKED_RESPONSES, logprobs_results=None)
+    assert list(set(uqe.logprobs)) == [None]
+    assert list(set(sum(uqe.multiple_logprobs, []))) == [None]
