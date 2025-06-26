a9e9b12f67a3effba2f1806f3c73f6ec51801bc9
Jinesh Mehta <mehtajineshs@gmail.com>
2025-06-26 11:29:58 -0500

    updated sentence transformer import location

--- uqlm/black_box/cosine.py
+++ uqlm/black_box/cosine.py
@@ -17,7 +17,6 @@ from typing import Any, List, Tuple
 
 import numpy as np
 from numpy.linalg import norm
-from sentence_transformers import SentenceTransformer
 
 from uqlm.black_box.baseclass.similarity_scorer import SimilarityScorer
 
@@ -33,8 +32,9 @@ class CosineScorer(SimilarityScorer):
             https://huggingface.co/sentence-transformers?sort_models=likes#models
             for more information. The recommended sentence transformer is 'all-MiniLM-L6-v2'.
         """
+        from sentence_transformers import SentenceTransformer
         self.transformer = transformer
-        self.model = SentenceTransformer(f"sentence-transformers/{transformer}")
+        self.model = SentenceTransformer(f"{transformer}")
 
     def evaluate(
         self, responses: List[str], sampled_responses: List[List[str]]
