708d70c1874bc349b3ed47fe393a367f0aeccbac
Dylan Bouchard <dylan.bouchard@cvshealth.com>
2026-01-02 19:39:59 +0000

    add networkx warning

--- uqlm/longform/graph/graph_scorer.py
+++ uqlm/longform/graph/graph_scorer.py
@@ -4,12 +4,16 @@ from rich.progress import Progress
 from uqlm.nli.nli import NLI
 import numpy as np
 import time
-import networkx as nx
 from scipy import sparse
 
 
 class GraphScorer(ClaimScorer):
     def __init__(self, nli_model_name: Optional[str] = "microsoft/deberta-large-mnli", device: Optional[Any] = None, max_length: Optional[int] = 2000) -> None:
+        try:
+            import networkx as nx
+        except ImportError:
+            print("graph-based scoring requires `networkx` package. Please install using `pip install networkx`.")
+        
         self.nli_model_name = nli_model_name
         self.device = device
         self.max_length = max_length
