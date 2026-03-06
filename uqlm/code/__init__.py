6789f8eb605992b015397c45843e6871e9123c6b
2613037 <mohitcek@gmail.com>
2026-03-06 09:55:38 -0500

    remove codegen version of cosine scorer

--- uqlm/code/__init__.py
+++ uqlm/code/__init__.py
@@ -2,8 +2,7 @@ from uqlm.code.codeequivalence import CodeEquivalence
 from uqlm.code.codebleu import CodeBLEU
 from uqlm.code.verbalizedconfidence import VerbalizedConfidence
 from uqlm.code.entropy import FunctionalEntropy
-from uqlm.code.cosine import CosineScorer
 from uqlm.code.clusterer import CodeClusterer
 
 
-__all__ = ["CodeEquivalence", "CodeBLEU", "VerbalizedConfidence", "FunctionalEntropy", "CosineScorer", "CodeClusterer"]
+__all__ = ["CodeEquivalence", "CodeBLEU", "VerbalizedConfidence", "FunctionalEntropy", "CodeClusterer"]
