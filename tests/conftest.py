7e702178e406925f2d7e00a23d9996ed5705cf73
Zeya Ahmad <zeya.ahmad@cvshealth.com>
2026-03-27 13:09:09 -0400

    update unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os, platform
+import pytest
 
 # disable MPS on macOS to prevent all OOM failures.
 import torch
@@ -8,12 +9,14 @@ if hasattr(torch.backends, "mps"):
     torch.backends.mps.is_built = lambda: False
 
 
-# Automatically SKIP ALL transformer-heavy tests on Linux CI and Windows
-def pytest_runtest_setup(item):
-    system = platform.system()
-    if system == "Windows" or (os.getenv("CI") == "true" and system == "Linux"):
-        # Only skip the tests needing transformer models
-        if "test_blackboxuq" in item.location[0] or "test_codegen" in item.location[0] or "test_similarity" in item.location[0] or "test_sampled_logprobs" in item.location[0] or "test_longtextuq" in item.location[0] or "test_longtextqa" in item.location[0]:
-            import pytest
+HEAVY_TESTS = ["test_blackboxuq", "test_codegen", "test_similarity", "test_sampled_logprobs", "test_longtextuq", "test_longtextqa"]
 
-            pytest.skip("Skipping heavy transformer tests on Windows / Linux CI")
+
+# Automatically Skip ALL transformer-heavy tests
+def pytest_runtest_setup(item):
+    filename = item.location[0]
+    system = platform.system()
+    # Skip on Windows, Linux, and macOS
+    if system in ("Windows", "Linux", "Darwin"):
+        if any(test in filename for test in HEAVY_TESTS):
+            pytest.skip("Skipping heavy transformer tests on all 3 platforms")
